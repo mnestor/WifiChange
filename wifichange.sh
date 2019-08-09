99efcc2f6c7146b5eac2f2d8470ee26796df1e3c
Mike Nestor <mnestor79@gmail.com>
2019-08-09 14:27:20 -0400

    and add disconnected state

--- wifichange.sh
+++ wifichange.sh
@@ -3,6 +3,14 @@
 SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 SSID=`/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I | awk '/ SSID/ {ssid = tolower(substr($0, index($0, $2))); sub(/ /, "_", ssid);print ssid}'`
 
+if [ "$SSID" == "" ]; then
+  DISCONNECTED="${SCRIPTPATH}/disconnected"
+  if [ -f $DISCONNECTED ]; then
+    sh $DISCONNECTED
+  fi
+  exit 0
+fi
+
 SCRIPT="${SCRIPTPATH}/connect/${SSID}"
 
 # do we have a script for the current wifi?
@@ -12,8 +20,8 @@ if [ -f $SCRIPT ]; then
 fi
 
 # if we don't find for the current, do we have a default?
-SCRITP_DEFAULT="${SCRIPTPATH}/default.noscript"
-if [ -f $SCRITP_DEFAULT ]; then
-  sh $SCRITP_DEFAULT
+SCRIPT_DEFAULT="${SCRIPTPATH}/default.noscript"
+if [ -f $SCRIPT_DEFAULT ]; then
+  sh $SCRIPT_DEFAULT
   exit 0
 fi
